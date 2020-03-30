c3d767d9c420a4162c7a354f517795d9c8b9a02f
PaolaS2992 <huaripayta29.92@gmail.com>
2020-03-30 18:39:45 -0500

    funciones validateEmail, getPagination, getIdOrEmail

---
+++ utils/utils.js
@@ -0,0 +1,28 @@
+const { ObjectId } = require('mongodb');
+
+module.exports.validateEmail = (email) => {
+    const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(String(email).toLowerCase());
+};
+
+module.exports.getPagination = (url, page, limit, numbersPages) => {
+    const firstPage = `<${url}?limit=${limit}&page=${1}>; rel="first"`;
+    // console.log('firstPage', firstPage);
+    const prevPage = `<${url}?limit=${limit}&page=${page - 1}>; rel="prev"`;
+    // console.log('prevPage', prevPage);
+    const nextPage = `<${url}?limit=${limit}&page=${page + 1}>; rel="next"`;
+    // console.log('nextPage', nextPage);
+    const lastPage = `<${url}?limit=${limit}&page=${numbersPages}>; rel="last"`;
+    // console.log('lastPage', lastPage);
+    return `${firstPage}, ${prevPage}, ${nextPage}, ${lastPage}`;
+  };
+
+  module.exports.getIdOrEmail = (uid) => {
+    const queryUid = {};
+    if (!(this.validateEmail(uid))) {
+      queryUid._id = new ObjectId(uid);
+    } else {
+      queryUid.email = uid;
+    }
+    return queryUid;
+  };
