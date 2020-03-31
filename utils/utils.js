09916ef7338bf69f3f21216a4b492ddce905b983
PaolaS2992 <huaripayta29.92@gmail.com>
2020-03-31 13:02:57 -0500

    implementa la funcion getProduct, para mostrar todas las propiedades de un producto - name, price, imagen, categoria

--- utils/utils.js
+++ utils/utils.js
@@ -1,4 +1,5 @@
 const { ObjectId } = require('mongodb');
+const collection = require('../connection/collection');
 
 module.exports.validateEmail = (email) => {
     const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -26,3 +27,20 @@ module.exports.getPagination = (url, page, limit, numbersPages) => {
     }
     return queryUid;
   };
+
+  module.exports.getProducts = (arrayIds, orderId, resp, next) => collection('products')
+    .then((collectionProducts) => collectionProducts.find({ _id: { $in: arrayIds } }).toArray()
+        .then((arrayProducts) => collection('orders')
+        .then((collectionOrders) => collectionOrders.findOne({ _id: new ObjectId(orderId) }))
+        .then((order) => {
+          if (order === null) {
+            return next(404);
+          }
+          // Renombramos propiedades en memoria de orders.
+          order.products = order.products.map((elemProduct) => ({
+            qty: elemProduct.qty,
+            product: arrayProducts.find((p) => p._id.equals(elemProduct.productId)),
+          }));
+          // console.log(order);
+          resp.send(order);
+        })));
