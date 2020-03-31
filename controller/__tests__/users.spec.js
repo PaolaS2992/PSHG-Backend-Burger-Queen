4f9b85b7f1d6097d78bac11757bc61b5c6e086c3
PaolaS2992 <huaripayta29.92@gmail.com>
2020-03-31 13:51:20 -0500

    realiza pruebas unitarias de users - funcion crear usuario

--- controller/__tests__/users.spec.js
+++ controller/__tests__/users.spec.js
@@ -1,6 +1,39 @@
+const database = require('../../connection/connectDB');
+
 const {
   getUsers,
+  createUser,
 } = require('../../controller/users');
+
+describe('createUser', () => {
+  beforeAll(async () => {
+    await database();
+  });
+  afterAll(async () => {
+    const collectionUsers = (await database()).collection('users');
+    await collectionUsers.deleteMany({});
+    await database().close();
+  });
+  it('should create a new user', (done) => {
+    const req = {
+      body: {
+        email: 'test1@test.com',
+        password: '123456',
+        roles: {
+          admin: false,
+        },
+      },
+    };
+    const resp = {
+      send: (response) => {
+        expect(response.email).toBe('test1@test.com');
+        expect(response.roles.admin).toBe(false);
+        done();
+      },
+    };
+    createUser(req, resp);
+  });
+});
 
 describe('getUsers', () => {
   it('should get users collection', (done) => {
